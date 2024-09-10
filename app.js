d093f7f962647ceacee02a59be87a592656d7eaa
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-09-10 19:56:06 +0500

    four

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const qrImage = document.getElementById("qr-image");
 qrButton.addEventListener("click", () => {
   let qrValue = qrInput.value;
   if (!qrValue) {
-    alert("please enter a value");
+    // alert("please enter a value");
+    swal("please enter a value");
   } else {
     qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example${qrValue}`;
     qrImage.alt = `QR Code for ${qrValue}`;
